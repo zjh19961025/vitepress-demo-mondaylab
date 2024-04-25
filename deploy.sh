a7fa922ddcb9b87e399a41b0b5a84a378c3dee6e
zjh19961025 <304795317@qq.com>
2024-04-25 10:36:39 +0800

    [测试自动部署]

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+pnpm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m '部署git-page'
+
+git push -f git@github.com:zjh19961025/vitepress-demo-mondaylab.git master:gh-pages
+
+# 返回上一级目录
+cd ..
+
+# 删除dist文件夹
+rm -rf dist
